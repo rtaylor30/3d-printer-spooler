57475178163f1acefb08a6a9fbb4254c75a64dbb
Rob Taylor <rtaylor30@gmail.com>
2013-06-02 16:50:29 -0400

    Initial project setup with Ruby on Rails

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-= 3D printing
+Web Platform for 3D Printing Spooling
+=====================================
+
+This is a a Ruby on Rails app that will interact with users to upload CAD files to print 3D models.
+This will communicate with another service that will manage the printing spooling.
 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web3dPrinterSpooler::Application.config.secret_token = '03d252dd340eebae842a46fcb3568c30ff5b7eae645f9af2d6fc416cd3a9fa9beb20934abb44fe3fb86fec6ee0c7cd974df1940f3cec68c6e46beae8dc22592d'
