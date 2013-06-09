d6d6a5f3c826d00b6774d42fbcc2c7ad613a9d37
Rob Taylor <rtaylor30@gmail.com>
2013-06-08 20:40:55 -0400

    when the status of a print request record has changed, send an email if it is completed

--- app/models/print_request.rb
+++ app/models/print_request.rb
@@ -2,5 +2,16 @@ class PrintRequest < ActiveRecord::Base
   belongs_to :printer
   belongs_to :print_job
   attr_accessible :status, :stl_file_name, :created_at
+  
+  after_commit :check_status_completed
+
+  private
+
+  def check_status_completed
+    if status == 'success'
+      PrintRequestStatusMailer.status_email(@user, self).deliver
+    end
+  end
+
 end
 
