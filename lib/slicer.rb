5c5ff1a24930970198b61674bca45fd4310762db
Rob Taylor <rtaylor30@gmail.com>
2013-07-21 19:41:19 -0400

    Corrected slicer path

--- lib/slicer.rb
+++ lib/slicer.rb
@@ -4,7 +4,7 @@ class Slicer
   class << self
     def run_slicer print_request_id
       print_request = PrintRequest.find print_request_id
-      cmd = "slic3r --load ~/Slic3r/3mm_pt5-nzl_pt3-lyr.ini #{print_request.stl_file_name}"
+      cmd = "slic3r --load #{Rails.root.join 'config', 'slicer_config.ini'} #{Rails.root.join 'public', 'stl_files', print_request.stl_file_name}"
       Open3.popen3 cmd { |stdin, stdout, stderr, wait_thr|
         if not stderr.read.blank?
           print_request.status = 'Error'
