b5078e907038a4584671b3eac00820cc0295b95e
Rob Taylor <rtaylor30@gmail.com>
2013-07-21 15:34:32 -0400

    Update the print request status depending on the run of the slicer

--- lib/slicer.rb
+++ lib/slicer.rb
@@ -6,7 +6,13 @@ class Slicer
       print_request = PrintRequest.find print_request_id
       cmd = "slic3r --load ~/Slic3r/3mm_pt5-nzl_pt3-lyr.ini #{print_request.stl_file_name}"
       Open3.popen3 cmd { |stdin, stdout, stderr, wait_thr|
-        # TODO : Do something when the slicer application doesn't run as expected
+        if not stderr.read.blank?
+          print_request.status = 'Error'
+        else
+          print_request.status = 'Not Ready'
+        end
+
+        print_request.save
       }
     end
     handle_asynchronously :run_slicer
